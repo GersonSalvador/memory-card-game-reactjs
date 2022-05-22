40c6e747819d65ac8da2a068114707e68a702679
Gerson Salvador <gerson.salvador@outlook.com>
2022-05-22 15:25:13 -0300

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import Routes from './routes';
+import GlobalStyle from "./styles/global"
 
 function App() {
   return (
     <div data-testid="app" className="App">
+      <GlobalStyle />
       <Router>
         <Routes/>
       </Router>
---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    transition: all 0.8s ease-in;
+  }
+
+  :root{
+    --Purple-1: #5C257F;
+    --Purple-2: #D697FF;
+    --Purple-3: #B84BFF;
+    --Purple-4: #6B4C7F;
+    --Purple-5: #933CCC;
+  }
+
+  html,body{
+    height: 100vh;
+  }
+
+  body{
+    background-color: var(--Purple-1);
+    font-family: sans-serif;
+  }
+`;
