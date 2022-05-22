c44a11c8b9e2cc2e642f762554cb3396e4e19bf7
Gerson Salvador <gerson.salvador@outlook.com>
2022-05-22 19:24:12 -0300

    feat: add styles to Title component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,12 +1,14 @@
+import { H1 } from "./styles"
+
 interface TitleProps {
   children?: string;
 };
 
 function Title({children}: TitleProps) {
   return (
-    <h1 data-testid="title">
+    <H1 data-testid="title">
       {children}
-    </h1>
+    </H1>
   )
 }
 
