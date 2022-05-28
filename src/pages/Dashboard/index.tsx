1b153492e2b679c03205156f54ab8fbdcf45fae6
Gerson Salvador <gerson.salvador@outlook.com>
2022-05-28 10:12:32 -0300

    feat: import Counter on Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Header, Main, Levels } from "../../components"
+import { Header, Main, Levels, Counter } from "../../components"
 import { ILevels } from "../../interfaces"
 
 const levels: ILevels[] = [
@@ -17,6 +17,7 @@ export default function Dashboard() {
       <Main>
         <Levels levels={levels}/>
       </Main>
+      <Counter timing={{hour: 0, minute: 0, second: 0}}/>
     </div>
   )
 }
