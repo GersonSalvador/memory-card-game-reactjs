b2e4fb1f1c734e89c56e09e08def74edcbcbb367
Gerson Salvador <gerson.salvador@outlook.com>
2022-05-28 10:55:34 -0300

    feat: create test for start state

--- src/components/Counter/counter.test.tsx
+++ src/components/Counter/counter.test.tsx
@@ -9,14 +9,31 @@ describe('Ul component', () => {
     expect(component).toBeInTheDocument();
   });
 
-  it("should render it's props", () => {
+  it("should show olny btn-start", () => {
+    const {queryByTestId} = render(<Counter timing={{hour: 1, minute: 5, second: 55}} />);
+
+    expect(queryByTestId('hour-value')).toBeNull();
+    expect(queryByTestId('minute-value')).toBeNull();
+    expect(queryByTestId('second-value')).toBeNull();
+    const startBtn = screen.getByTestId('btn-start');
+
+    expect(startBtn).toHaveTextContent('Start');
+  })
+
+  it("should render it's props and btn-start after btn-start been clicked", () => {
     render(<Counter timing={{hour: 1, minute: 5, second: 55}} />);
+
+    const startBtn = screen.getByTestId('btn-start');
+    startBtn.click()
+
     const hourProp = screen.getByTestId('hour-value');
     const minuteProp = screen.getByTestId('minute-value');
     const secondProp = screen.getByTestId('second-value');
+
     expect(hourProp).toHaveTextContent('1');
     expect(minuteProp).toHaveTextContent('5');
     expect(secondProp).toHaveTextContent('55');
+    expect(startBtn).toHaveTextContent('Stop');
   })
   
 })
