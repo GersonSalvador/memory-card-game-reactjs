6add97db89e8e8bcef42f770b78141900a3eae8d
Gerson Salvador <gerson.salvador@outlook.com>
2022-05-29 19:00:56 -0300

    feat: add the selected level to the Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,16 +3,17 @@ import { useState } from "react";
 import { Header, Main, Levels, Counter } from "../../components"
 import { ILevels } from "../../interfaces"
 
-const levels: ILevels[] = [
-  { name: 'Easy', pairs: 6 },
-  { name: 'Normal', pairs: 10 },
-  { name: 'Hard', pairs: 15 },
-  { name: 'Master', pairs: 25 },
-]
-
 export default function Dashboard() {
 
+  const [levelSelected, setLevelSelected] = useState(1);
   const [isStarted, setIsStarted] = useState(false);
+
+  const levels: ILevels[] = [
+    { name: 'Easy', pairs: 6, levelSelected, setLevelSelected },
+    { name: 'Normal', pairs: 10, levelSelected, setLevelSelected },
+    { name: 'Hard', pairs: 15, levelSelected, setLevelSelected },
+    { name: 'Master', pairs: 25, levelSelected, setLevelSelected },
+  ]
 
   return (
     <div data-testid="dashboard">
