c54752d8db526929d2e58ca574ed1d6c7d989685
Gerson Salvador <gerson.salvador@outlook.com>
2022-05-29 19:51:23 -0300

    feat: create test to check levels

--- src/pages/Dashboard/dashboard.test.tsx
+++ src/pages/Dashboard/dashboard.test.tsx
@@ -1,12 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Dashboard from './';
 
+
 describe('Dashboard Page', () => {
 
-  it('renders on screen ', () => {
+  function makeSut() {
     render(<Dashboard />);
+  }
+
+  it('renders on screen ', () => {
+    makeSut()
     const component = screen.getByTestId('dashboard');
     expect(component).toBeInTheDocument();
   });
+
+  it('shoukd have 4 levels', () => {
+    makeSut()
+    const component = screen.getByTestId('levels')
+    expect(component.childNodes.length).toEqual(4);
+  })
 
 })
