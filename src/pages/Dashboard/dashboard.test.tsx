bbd94c8bccdc4f514df51b6f5d2e6d90d974021b
Gerson Salvador <gerson.salvador@outlook.com>
2022-05-29 19:30:56 -0300

    refac: standard all tests' variables

--- src/pages/Dashboard/dashboard.test.tsx
+++ src/pages/Dashboard/dashboard.test.tsx
@@ -1,22 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import Dashboard from './';
 
-// function Button(){
-//   return <button data-testid="button-test">exemplo</button>
-// }
-
 describe('Dashboard Page', () => {
 
   it('renders on screen ', () => {
     render(<Dashboard />);
-    const dashboardElement = screen.getByTestId('dashboard');
-    expect(dashboardElement).toBeInTheDocument();
+    const component = screen.getByTestId('dashboard');
+    expect(component).toBeInTheDocument();
   });
-
-  // it("should render it's children", () => {
-  //   render(<Dashboard><Button /></Dashboard>);
-  //   const buttonElement = screen.getByTestId('button-test');
-  //   expect(buttonElement).toBeInTheDocument();
-  // })
 
 })
