5f1518f9686b48ebb4b7b8849735fd508ec1bd88
Gerson Salvador <gerson.salvador@outlook.com>
2023-01-09 20:22:52 -0300

    feat: test the wait state

--- src/components/Card/card.test.jsx
+++ src/components/Card/card.test.jsx
@@ -101,8 +101,55 @@ describe('Card', () => {
       }, 1000);
     });
 
-    //expect object to be exacly like this
+    it('updates the setWait state when the selected cards are not a pair', () => {
+      const setAllCards = jest.fn();
+      const setPair = jest.fn();
+      const cardInfo = { cardIndex: 0, isSelected: false, isHidden: false };
+      const wait = false;
+      const setWait = jest.fn();
+      const allCards = [cardInfo, cardInfo];
+      const { getByTestId } = render(<Card
+        cardInfo={cardInfo}
+        cardSequence={0}
+        allCards={allCards}
+        setAllCards={setAllCards}
+        pair={[1]}
+        setPair={setPair}
+        waitFn={{wait, setWait}}
+      />);
+      const cardElement = getByTestId('card');
+      fireEvent.click(cardElement);
+      expect(setWait).toHaveBeenCalledWith(true);
+      setTimeout(() => {
+        expect(setWait).toHaveBeenCalled(2);
+        expect(setWait).toHaveBeenCalledWith(false);
+      }, 1000);
+    });
 
+    it('updates the setWait state when the selected cards are a pair', () => {
+      const setAllCards = jest.fn();
+      const setPair = jest.fn();
+      const cardInfo = { cardIndex: 0, isSelected: false, isHidden: false };
+      const wait = false;
+      const setWait = jest.fn();
+      const allCards = [cardInfo, cardInfo];
+      const { getByTestId } = render(<Card
+        cardInfo={cardInfo}
+        cardSequence={0}
+        allCards={allCards}
+        setAllCards={setAllCards}
+        pair={[0]}
+        setPair={setPair}
+        waitFn={{wait, setWait}}
+      />);
+      const cardElement = getByTestId('card');
+      fireEvent.click(cardElement);
+      expect(setWait).toHaveBeenCalledWith(true);
+      setTimeout(() => {
+        expect(setWait).toHaveBeenCalled(2);
+        expect(setWait).toHaveBeenCalledWith(false);
+      }, 1000);
+    });
   });
 
 })
