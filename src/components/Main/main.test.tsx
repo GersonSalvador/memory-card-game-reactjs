a7f0ddaae7e95da4724c967ffe0d690468c5dc6c
Gerson Salvador <gerson.salvador@outlook.com>
2023-01-11 10:40:24 -0300

    refact: change test like others

--- src/components/Main/main.test.tsx
+++ src/components/Main/main.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Main from './';
 
 function ChildrenComponent(){
@@ -10,14 +10,14 @@ function ChildrenComponent(){
 describe('Header component', () => {
 
   it('renders on screen', () => {
-    render(<Main />);
-    const component = screen.getByTestId('main');
+    const {getByTestId} = render(<Main />);
+    const component = getByTestId('main');
     expect(component).toBeInTheDocument();
   });
 
   it("should render it's children", () => {
-    render(<Main><ChildrenComponent /></Main>);
-    const children = screen.getByTestId('span');
+    const {getByTestId} = render(<Main><ChildrenComponent /></Main>);
+    const children = getByTestId('span');
     expect(children).toBeInTheDocument();
   })
   
