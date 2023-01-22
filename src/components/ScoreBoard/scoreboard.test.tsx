d893ff217b7fa3ec0add3cf890ba5beeab26ec4f
Gerson Salvador <gerson.salvador@outlook.com>
2023-01-22 16:22:51 -0300

    feat: create ScoreBoard component test

--- src/components/ScoreBoard/scoreboard.test.tsx
+++ src/components/ScoreBoard/scoreboard.test.tsx
@@ -3,18 +3,29 @@ import ScoreBoard from '.';
 
 describe('', () => {
   const makeSut = () => {
-    const time = {
-      seconds: 0,
-      minutes: 0,
-      hours: 0,
+    const scores = [{
+      player: 'Fake Name',
+      time: {
+        seconds: 2,
+        minutes: 1,
+        hours: 0,
+      }
+    }]
+    const props = {
+      scores
     }
-    return render(<ScoreBoard isStarted
-      isWon
-      time={time}/>)
+    return render(<ScoreBoard {...props}/>)
   }
 
   it('should render on screen', () => {
     const {getByTestId} = makeSut()
     expect(getByTestId('score-board')).toBeInTheDocument()
   })
+
+  it('should have 1 score on the board list', () => {
+    const {getAllByTestId} = makeSut()
+    const scores = getAllByTestId('score')
+    expect(scores.length).toBe(1)
+  })
+
 })
